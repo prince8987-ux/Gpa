cbdd202f696da7e11d1b7d8d8a78bbb7de52474d
prince <princekumar29.04.2008@gmail.com>
2026-04-21 16:41:50 +0530

    update hello.java

---
+++ hello.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class hello
+{
+  public static void main(String args[])
+  {
+    int a;
+    System.out.println("enter your no:");
+    Scanner x= new Scanner(System.in);
+    a= x.nextInt();
+    System.out.println("your no"+a);
+  }
+}
